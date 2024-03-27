18fee2190de3cf8958cb10be660251a6d3c856bd
lukitas0606 <nicolascontrerasuribe0@gmail.com>
2024-03-27 20:40:52 -0300

    Modificación de Navbar y Footer

--- src/shared/components/Footer.jsx
+++ src/shared/components/Footer.jsx
@@ -1,35 +1,65 @@
+import { Typography } from '@material-tailwind/react'
+
+const DEVELOPERS = [
+  'Jonathan Araos',
+  'Ayxa Chaverra',
+  'Cristobal Walters',
+  'Nicolás Contreras'
+]
+
+const INSTRUCTORS = [
+  'Emiliano Rotta',
+  'Fabian Pino',
+  'Loreto Bustos',
+  'Raúl Farias'
+]
+
 export function Footer () {
+  const currentYear = new Date().getFullYear()
+
   return (
-        <footer className="bg-black text-white py-6">
-            <div className="container mx-auto px-4 flex flex-col items-center">
-                <div className="flex flex-col md:flex-row justify-center md:space-x-12 mb-4">
-                    <div className="text-center mb-8 md:mb-0">
-                        <h2 className="text-lg font-bold mb-4">JUNIORS.TECH</h2>
-                        <p className="mb-2">Acerca del proyecto</p>
-                        <p className="mb-2">Políticas de privacidad</p>
-                        <p className="mb-2">Términos de uso</p>
-                    </div>
-                    <div className="text-center mb-8 md:mb-0">
-                        <h2 className="text-lg font-bold mb-4">
-                            DESARROLLADORES
-                        </h2>
-                        <p className="mb-2">Jonathan Araos</p>
-                        <p className="mb-2">Ayxa Chaverra</p>
-                        <p className="mb-2">Cristobal Walters</p>
-                        <p className="mb-2">Nicolás Contreras</p>
-                    </div>
-                    <div className="text-center">
-                        <h2 className="text-lg font-bold mb-4">INSTRUCTORES</h2>
-                        <p className="mb-2">Emiliano Rotta</p>
-                        <p className="mb-2">Fabian Pino</p>
-                        <p className="mb-2">Loreto Bustos</p>
-                        <p className="mb-2">Raúl Farias</p>
-                    </div>
-                </div>
-                <p className="text-center mt-8">
-                    © 2024 Todos los derechos reservados. Juniors.TECH
-                </p>
+    <footer className="bg-black text-white py-6">
+      <div className="container mx-auto px-4 flex flex-col items-center">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-12 mb-4">
+          <div className="text-center">
+            <Typography variant="lg" className="font-bold mb-4">
+              JUNIORS.TECH
+            </Typography>
+            <ul className="space-y-2">
+              <Typography as="li">Acerca del proyecto</Typography>
+              <Typography as="li">Políticas de privacidad</Typography>
+              <Typography as="li">Términos de uso</Typography>
+            </ul>
+          </div>
+          <div className="text-center">
+            <Typography variant="lg" className="font-bold mb-4">
+              DESARROLLADORES
+            </Typography>
+            <div className="grid grid-cols-1 gap-2">
+              {DEVELOPERS.map((developer, index) => (
+                <Typography key={index} as="p">
+                  {developer}
+                </Typography>
+              ))}
             </div>
-        </footer>
+          </div>
+          <div className="text-center">
+            <Typography variant="lg" className="font-bold mb-4">
+              INSTRUCTORES
+            </Typography>
+            <div className="grid grid-cols-1 gap-2">
+              {INSTRUCTORS.map((instructor, index) => (
+                <Typography key={index} as="p">
+                  {instructor}
+                </Typography>
+              ))}
+            </div>
+          </div>
+        </div>
+        <p className="text-center mt-8">
+          &copy; {currentYear} Todos los derechos reservados. Juniors.TECH
+        </p>
+      </div>
+    </footer>
   )
 }
--- src/shared/components/Navbar.jsx
+++ src/shared/components/Navbar.jsx
@@ -27,7 +27,7 @@ export function NavbarLanding () {
   }, [])
 
   return (
-        <Navbar className="mx-auto max-w-screen-xl px-4 py-2">
+        <Navbar className="max-w-full mx-auto w-full px-4 py-2">
             <div className="flex items-center justify-between text-blue-gray-900">
                 <Typography
                     as="a"
