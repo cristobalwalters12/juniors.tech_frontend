01ff4195497709a07c40cbb03daddb6811e0b532
cristobalwalters12 <cristobalwalters@gmail.com>
2024-03-28 13:34:49 -0300

    Agrega configuración de mocks y handlers para pruebas de unidad.

--- src/main.jsx
+++ src/main.jsx
@@ -9,18 +9,22 @@ import { ThemeProvider } from '@material-tailwind/react'
 import App from './App.jsx'
 import './index.css'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
+import { enableMocks } from './config/enviroment.js'
 const queryClient = new QueryClient()
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ThemeProvider>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-        <ReactQueryDevtools/>
-      </QueryClientProvider>
-    </ThemeProvider>
-  </React.StrictMode>
-)
+enableMocks().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+          <ReactQueryDevtools/>
+        </QueryClientProvider>
+      </ThemeProvider>
+    </React.StrictMode>
+  )
+}).catch((error) => {
+  console.error('[main.jsx] ==> enableMocking.', error)
+})
