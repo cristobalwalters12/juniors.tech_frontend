ea7ca57b817db5076837139812982089c0252f9d
cristobalwalters12 <cristobalwalters@gmail.com>
2024-03-29 00:19:22 -0300

    Agregadas rutas de inicio de sesión y perfil público

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Home from './pages/Home'
 import PostDetails from './pages/PostDetails'
 import Register from './pages/Register'
 import RegisterEmail from './pages/RegisterEmail'
+import Login from './pages/login'
+import PublicProfile from './pages/PublicProfile'
 
 function App () {
   return (
@@ -11,6 +13,8 @@ function App () {
       <Route path="/post" element={<PostDetails />} />
       <Route path="/Register"element= {<Register/>}/>
       <Route path="/RegisterEmail"element= {<RegisterEmail/>}/>
+      <Route path="/Login"element= {<Login/>}/>
+      <Route path="/PublicProfile"element= {<PublicProfile/>}/>
 
     </Routes>
   )
