2c420d226060819bd0d19ebbdb33fbcdb0d14be0
cristobalwalters12 <cristobalwalters@gmail.com>
2024-03-29 00:19:06 -0300

    Actualizar rutas de API y mensajes de error en mockUsers.js

--- __mocks__/store/mockUsers.js
+++ __mocks__/store/mockUsers.js
@@ -1,6 +1,6 @@
 import { http, HttpResponse } from 'msw'
 
-const getUsers = http.get('/users', () => {
+const getUsers = http.get('api/v1/users', () => {
   return HttpResponse.json({
     users: [
       { id: 1, name: 'John Doe', username: 'johndoe', email: 'jhonDoe@gmail.com', password: '123456' },
@@ -15,22 +15,24 @@ const arrayUsers = [
   { id: 1, name: 'John Doe', username: 'johndoe', email: '', password: '' },
   { id: 2, name: 'Jane Doe', username: 'janedoe', email: '', password: '' }
 ]
-const addUsers = http.post('/users', async ({ request }) => {
+const addUsers = http.post('/api/v1/sign-up', async ({ request }) => {
   const user = await request.json()
   const email = user.email ?? ''
   const username = user.username ?? ''
   const password = user.password ?? ''
+  const birthdate = user.birthdate ?? ''
 
-  if (email === '' || username === '' || password === '') {
-    return HttpResponse.json({ error: 'Invalid user' }, { status: 400 })
+  if (email === '' || username === '' || password === '' || birthdate === '') {
+    return HttpResponse.json({ error: 'este usuario no es valido' }, { status: 400 })
   }
 
   if (arrayUsers.some(u => u.username === username)) {
-    return HttpResponse.json({ error: 'Username already exists' }, { status: 404 })
+    return HttpResponse.json({ error: 'Username already exists' }, { status: 405 })
   }
+  return HttpResponse.json({ message: 'User created' }, { status: 201 })
 })
 
-const login = http.post('/login', async ({ request }) => {
+const login = http.post('api/v1/login', async ({ request }) => {
   const user = await request.json()
   const username = user.username ?? ''
   const password = user.password ?? ''
@@ -42,8 +44,7 @@ const login = http.post('/login', async ({ request }) => {
   if (!arrayUsers.some(u => u.username === username && u.password === password)) {
     return HttpResponse.json({ error: 'Invalid credentials' }, { status: 404 })
   }
-
-  return HttpResponse.json({ token: 'token' }, { status: 200 })
+  HttpResponse.json({ token: 'token' }, { status: 200 })
 })
 
 export default [getUsers, addUsers, login]
