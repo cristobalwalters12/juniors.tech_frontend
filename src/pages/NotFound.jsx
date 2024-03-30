eaf655a5e97f125ab2c4dd092b1c40a87e31a549
Ayxa Chaverra <achaverrar@gmail.com>
2024-03-29 20:19:12 -0500

    fix prop-value mismatch in button in the 'not found' page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -19,7 +19,7 @@ function NavList () {
       <Link to="/login" >
         <Button
           as="li"
-          variant="small"
+          size="md"
           color="blue-gray"
           className="p-1 mr-16 font-medium"
         >
