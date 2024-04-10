963d6ff163dbbec13b167e1e382122e364eddc0a
Ayxa Chaverra <achaverrar@gmail.com>
2024-04-10 16:07:48 -0500

    fix typo in the RequireAuth component to correctly get the current user's roles

--- src/shared/components/RequireAuth.jsx
+++ src/shared/components/RequireAuth.jsx
@@ -3,10 +3,10 @@ import { useAuthStore } from '../../stores/authStore'
 
 const RequireAuth = ({ allowedRoles }) => {
   const isAuth = useAuthStore((state) => state.isAuth)
-  const role = useAuthStore((state) => state.role)
+  const roles = useAuthStore((state) => state.roles)
   const location = useLocation()
   return (
-    isAuth && role?.find(role => allowedRoles?.includes(role))
+    isAuth && roles?.find(role => allowedRoles?.includes(role))
       ? <Outlet />
       : <Navigate to='/login' state={{ from: location }} replace />
   )
