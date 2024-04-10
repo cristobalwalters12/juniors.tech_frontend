3adeedcca0eb5e29357cbd180e4c864054eabf95
Ayxa Chaverra <achaverrar@gmail.com>
2024-04-10 15:31:10 -0500

    adapt the Post service and component to the new API to allow users view a single post given its id

--- src/services/posts.js
+++ src/services/posts.js
@@ -12,8 +12,24 @@ const createPost = async (post) => {
 }
 
 const getPostById = async (id) => {
-  const { data } = await baseApi.get(`${API_PATHS.posts}/${id}`)
-  return data
+  const { data: { data: post } } = await baseApi.get(`${API_PATHS.posts}/${id}`)
+  return {
+    id: post.id,
+    title: post.title,
+    body: post.body,
+    categoryId: post.categoryId,
+    category: post.category,
+    slug: post.slug,
+    authorId: post.authorId,
+    authorUsername: post.username,
+    avatarUrl: post.avatarUrl,
+    voteCount: post.voteCount,
+    commentCount: post.commentCount,
+    createdAt: post.createdAt,
+    updatedAt: post.updatedAt,
+    hasOpenReport: post.hasOpenReport,
+    voteDirection: post.voteDirection
+  }
 }
 
 const editPost = async ({ id, post }) => await baseApi.put(`${API_PATHS.posts}/${id}`, post)
