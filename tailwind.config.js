5eb1a0cbcc1afb001cf6d601b52890a39aadf54d
Ayxa Chaverra <achaverrar@gmail.com>
2024-04-14 11:05:07 -0500

    add custom colors to the tailwind configuration file

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,21 @@ export default withMT({
         md: '768px',
         lg: '1024px',
         xl: '1280px'
+      },
+      colors: {
+        accent: {
+          dark: '#0F171E',
+          dhover: '#0D1A4E',
+          light: '#FFFFFF'
+        },
+        primary: {
+          dark: '#1C37A5',
+          light: '#A5B2E9'
+        },
+        grey: {
+          dark: '#8291A1',
+          light: '#DEE7EE'
+        }
       }
     }
   },
