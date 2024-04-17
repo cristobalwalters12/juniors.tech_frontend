63e8083fcd1be3c27f01250beeca663274c692cc
Ayxa Chaverra <achaverrar@gmail.com>
2024-04-16 23:43:56 -0500

    use toats to handle errors in the post details page and set the document title to that of the post

--- src/pages/PostDetails.jsx
+++ src/pages/PostDetails.jsx
@@ -6,22 +6,30 @@ import { Post } from '../features/posts/Post'
 import { CommentList } from '../features/comments/CommentList'
 import { SaveCommentForm } from '../features/comments/SaveCommentForm'
 import RequireAuthOnClick from '../shared/components/Auth/RequireAuthOnClick'
+import { useDocumentTitle } from '../shared/hooks/useDocumentTitle'
+import { showErrorToast } from '../shared/utils/showErrorToast'
 
 const PostDetails = () => {
   const { id: postId } = useParams()
   const [replying, setReplying] = useState(false)
-  const query = useGetSinglePost(postId)
+  const getSinglePostQuery = useGetSinglePost(postId)
+  useDocumentTitle(getSinglePostQuery.data?.title, getSinglePostQuery.isFetched)
+
   const toggleReplyForm = () => setReplying(prevState => !prevState)
 
-  if (query.isLoading) return <h1>Cargando...</h1>
-  if (query.isError) {
-    return <Navigate to="/not-found" />
+  if (getSinglePostQuery.isLoading) return <h1>Cargando...</h1>
+  if (getSinglePostQuery.isError) {
+    const { error } = getSinglePostQuery
+    showErrorToast(error, 'Error al cargar la publicación')
+    if (error.status === 404) {
+      return <Navigate to="/not-found" />
+    }
   }
   const newComment = { postId, parentId: postId }
 
   return (
       <div className='max-w-[60rem]'>
-        <Post post={query.data} />
+        <Post post={getSinglePostQuery.data} />
         <div className='pt-4 pl-0 '>
           {
             replying
