07ba9910d64cd476d9939c5695fe30fe018de800
Ayxa Chaverra <achaverrar@gmail.com>
2024-04-16 22:26:46 -0500

    better cache the comments fetched from the API using a more specific query key

--- src/features/comments/useGetComments.js
+++ src/features/comments/useGetComments.js
@@ -1,12 +1,11 @@
 import { useQuery } from '@tanstack/react-query'
 import { getComments } from '../../services/comments'
 
-const useGetComments = (id) => {
+const useGetComments = (postId) => {
   return useQuery({
-    queryKey: ['comments'],
-    queryFn: () => getComments(id),
+    queryKey: ['comments', postId],
+    queryFn: () => getComments(postId),
     staleTime: 60 * 60 * 1000 // 1h in ms
-    // select: (data) => data?.sort((a, b) => a.id - b.id)
   })
 }
 
