c8224adc6f430d7aee7d295c3e0418b64ad233bc
Ayxa Chaverra <achaverrar@gmail.com>
2024-04-17 10:52:01 -0500

    improve the show error toast function to leverage the error messages from the API

--- src/shared/utils/showErrorToast.js
+++ src/shared/utils/showErrorToast.js
@@ -4,8 +4,13 @@ const ERROR_CODES = {
   ERR_BAD_REQUEST: 'Recurso no encontrado',
   ERR_NETWORK: 'Error de conexión'
 }
-const showErrorToast = ({ code, message }, customMessage) => {
-  const toastMessage = ERROR_CODES[code] || customMessage || message
+const showErrorToast = ({ code, response: { status, data: { message } } }, customMessage) => {
+  let toastMessage = ''
+  if (status >= 400 && status < 500) {
+    toastMessage = message
+  } else {
+    toastMessage = ERROR_CODES[code] || customMessage || message
+  }
   toast.error(toastMessage)
 }
 
