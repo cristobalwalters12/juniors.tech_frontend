2d8f9ec809776a1a8fe7d8e2b727ba46b7b01c8e
Ayxa Chaverra <achaverrar@gmail.com>
2024-04-22 20:08:28 -0500

    implement an inifinite scroll navigation on the home page

--- src/features/posts/useGetPosts.js
+++ src/features/posts/useGetPosts.js
@@ -1,11 +1,18 @@
-import { useQuery } from '@tanstack/react-query'
+import { useInfiniteQuery } from '@tanstack/react-query'
 import { getPosts } from '../../services/posts'
+import { useSearchParams } from 'react-router-dom'
 
 const useGetPosts = () => {
-  return useQuery({
-    queryKey: ['posts'],
-    queryFn: getPosts
-    // select: (data) => data.sort((a, b) => b.id - a.id)
+  const [searchParams] = useSearchParams()
+  const sort = searchParams.get('sort')
+  const order = searchParams.get('order')
+  return useInfiniteQuery({
+    queryKey: ['posts', { sort, order }],
+    queryFn: ({ pageParam }) => getPosts({ sort, order, page: pageParam }),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) => lastPage.nextPage,
+    staleTime: 5 * 60 * 1000,
+    refetchOnWindowFocus: false
   })
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,71 @@
-import { Navigate } from 'react-router-dom'
-import PostList from '../features/posts/PostList'
-import { useGetPosts } from '../features/posts/useGetPosts'
+import { Link } from 'react-router-dom'
 import { useDocumentTitle } from '../shared/hooks/useDocumentTitle'
+import { useGetPosts } from '../features/posts/useGetPosts'
+import PostSummary from '../features/posts/PostSummary'
+import { useCallback, useRef } from 'react'
+import { Spinner } from '@material-tailwind/react'
 
 const Home = () => {
-  const getPostsQuery = useGetPosts()
-  useDocumentTitle('Home - Juniors.tech')
+  useDocumentTitle('Home')
+  const {
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    data,
+    status,
+    error
+  } = useGetPosts()
 
-  if (getPostsQuery.isLoading) {
-    return <h2>Cargando...</h2>
+  const intObserver = useRef()
+  const lastPostRef = useCallback(post => {
+    if (isFetchingNextPage) return
+
+    if (intObserver.current) intObserver.current.disconnect()
+
+    intObserver.current = new IntersectionObserver(posts => {
+      if (posts[0].isIntersecting && hasNextPage) {
+        fetchNextPage()
+      }
+    })
+
+    if (post) intObserver.current.observe(post)
+  }, [isFetchingNextPage, fetchNextPage, hasNextPage])
+
+  if (status === 'loading') {
+    return (
+    <div className='flex justify-center'>
+      <Spinner className="h-16 w-16 text-gray-900/50" />
+    </div>
+    )
   }
 
-  if (getPostsQuery.isError) {
-    return <Navigate to="not-found"/>
+  if (status === 'error') {
+    return <div>Error: {error.message}</div>
   }
+
+  const posts = data?.pages.flatMap(page => page.posts) || []
 
   return (
-    <div className="min-h-screen">
-      <PostList {...getPostsQuery.data} />
+    <div className='min-h-full flex flex-col gap-3 max-w-[48rem] mb-4 mr-4'>
+      {posts.map((post, index, { length }) => {
+        if (index === length - 1) {
+          return (
+          <Link ref={lastPostRef} key={post.id} to={`/posts/${post.id}/${post.slug}`}>
+            <PostSummary post={post} />
+          </Link>
+          )
+        }
+        return (
+          <Link key={post.id} to={`/posts/${post.id}/${post.slug}`}>
+            <PostSummary post={post} />
+          </Link>
+        )
+      })}
+      {isFetchingNextPage && (
+        <div className='flex justify-center'>
+          <Spinner className="h-16 w-16 text-gray-900/50" />
+        </div>
+      )}
     </div>
   )
 }
