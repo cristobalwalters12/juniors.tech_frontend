2b6145fee15cacafd08fc19df59616b4e5b9ddef
Ayxa Chaverra <achaverrar@gmail.com>
2024-04-24 18:32:17 -0500

    move the UserSearch component to the pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -25,10 +25,10 @@ import PersistAuth from './shared/components/Auth/PersistAuth'
 import RequireAuth from './shared/components/Auth/RequireAuth'
 import AuthenticateForm from './shared/components/Auth/AuthenticateForm'
 import RequireAccountOwner from './shared/components/Auth/RequireAccountOwner'
-import UserSearch from './features/users/UserSearch'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Home from './pages/Home'
+import UserSearch from './pages/UserSearch'
 
 function App () {
   return (
