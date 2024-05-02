6d47984e457b6674c7a5a5fea82bb90c69e2546b
Ayxa Chaverra <achaverrar@gmail.com>
2024-05-01 19:02:21 -0500

    use rich text viewer to display comments in the post details page

--- src/features/comments/Comment.jsx
+++ src/features/comments/Comment.jsx
@@ -3,7 +3,6 @@ import {
   CardBody,
   CardHeader,
   Collapse,
-  // Collapse,
   Typography
 } from '@material-tailwind/react'
 import { useState } from 'react'
@@ -16,6 +15,7 @@ import { useDeleteComment } from './useDeleteComment'
 import { showErrorToast } from '../../shared/utils/showErrorToast'
 import { toast } from 'react-toastify'
 import { useVoteComment } from './useVoteComment'
+import ContentViewer from '../../shared/components/TextEditors/ContentViewer'
 
 const Comment = ({ comment, getRepliesById }) => {
   const [editing, setEditing] = useState(false)
@@ -40,6 +40,7 @@ const Comment = ({ comment, getRepliesById }) => {
       showErrorToast(err, 'Error al intentar votar')
     })
   }
+
   const handleReport = () => {}
 
   const handleDelete = () => {
@@ -66,7 +67,7 @@ const Comment = ({ comment, getRepliesById }) => {
         color="transparent"
         shadow={false}
         onClick={toggleReplies}
-        className={`w-full mt-3 p-3 pb-2 bg-blue-gray-50 ${comment.commentCount > 0 ? 'cursor-pointer' : ''}`}
+        className={`w-full mt-3 p-3 pb-2 bg-grey-lighter ${comment.commentCount > 0 ? 'cursor-pointer' : ''}`}
       >
         <CardHeader
           color="transparent"
@@ -94,17 +95,17 @@ const Comment = ({ comment, getRepliesById }) => {
             )}
           </div>
         </CardHeader>
-        <CardBody className="mb-0 p-0 pr-2 ml-11">
+        <CardBody className="mb-0 p-0 pr-2">
         {
           editing
-            ? <SaveCommentForm
-                onClose={hideEditingForm}
-                comment={comment}
-              />
-            : (<>
-                <Typography variant='small' className={`font-normal ${comment.deletedAt ? 'text-gray-600' : ''}`}>
-                  {comment.body}
-                </Typography>
+            ? <div className="ml-7">
+                <SaveCommentForm onClose={hideEditingForm} comment={comment} />
+              </div>
+            : (<div className='ml-11'>
+                <ContentViewer
+                  body={comment.body}
+                  className={`${comment.deletedAt} ? 'ql-content-deleted' : ''`}
+                />
                 <CustomCardFooter
                   comment={comment}
                   onEdit={showEditingForm}
@@ -114,7 +115,7 @@ const Comment = ({ comment, getRepliesById }) => {
                   onReport={handleReport}
                   onDelete={handleDelete}
                 />
-              </>)
+              </div>)
         }
         </CardBody>
       </Card>
