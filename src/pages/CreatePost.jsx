7e2bae884a1288814ace4aa5160b6e731c1f81f0
Ayxa Chaverra <achaverrar@gmail.com>
2024-05-03 09:19:48 -0500

    add document title to the create post page

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,7 +1,9 @@
 import { Card, Typography } from '@material-tailwind/react'
 import SavePostForm from '../features/posts/SavePostForm'
+import { useDocumentTitle } from 'usehooks-ts'
 
 const CreatePost = () => {
+  useDocumentTitle('Crea una nueva publicación')
   return (
     <Card className='p-8 pb-6 mb-4 mr-4 bg-accent-light max-w-[48rem]'>
       <Typography variant="h4" color="blue-gray">
