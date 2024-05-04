b55b84677be3e2aa2495cbce316c698abfee3cb2
Ayxa Chaverra <achaverrar@gmail.com>
2024-05-04 15:15:34 -0500

    update the post's comment count when the a new comment is created

--- src/features/comments/useSaveComment.js
+++ src/features/comments/useSaveComment.js
@@ -7,8 +7,12 @@ const useSaveComment = () => {
     mutationFn: saveComment,
     onMutate: (payload) => payload,
     onSuccess: (savedComment, variables, context) => {
+      let newComment = true
       queryClient.setQueryData(['posts', savedComment.postId, 'comments'], (prevComments) => {
+        // Create the first comment on a post
         if (!prevComments) return [savedComment]
+
+        // Comment on a post, when there are other comments
         if (!context.commentId) {
           const updatedComments = prevComments.map(comment => {
             if (comment.id !== savedComment.parentId) return comment
@@ -19,7 +23,16 @@ const useSaveComment = () => {
           })
           return [savedComment, ...updatedComments]
         }
+
+        // Edit an existing comment
+        newComment = false
         return prevComments.map(comment => comment.id === savedComment.id ? savedComment : comment)
+      })
+
+      if (!newComment) return
+      queryClient.setQueryData(['posts', savedComment.postId], (prevPost) => {
+        prevPost.commentCount += 1
+        return prevPost
       })
     },
     retry: 0
