e0d2de6b29044d6c4ac53ea0b326bea45e2f5eb1
Ayxa Chaverra <achaverrar@gmail.com>
2024-05-04 17:35:44 -0500

    use optimistic updates to improve the user experience while voting on comments

--- src/features/comments/useVoteComment.js
+++ src/features/comments/useVoteComment.js
@@ -6,38 +6,46 @@ const useVoteComment = () => {
 
   return useMutation({
     mutationFn: voteComment,
-    onMutate: (payload) => payload,
-    onSuccess: (votedComment, variables, context) => {
+    onMutate: (payload) => {
+      let prevCommentsData
       queryClient.setQueryData(
-        ['posts', context.postId, 'comments'],
-        (prevComments) => (
-          prevComments.map(comment => {
-            if (comment.id !== context.commentId) {
+        ['posts', payload.postId, 'comments'],
+        (prevComments) => {
+          prevCommentsData = prevComments
+          return prevComments.map(comment => {
+            if (comment.id !== payload.commentId) {
               return comment
             }
             let newVoteCount = 0
             let newVoteDirection = 0
             // unvote
-            if (comment.voteDirection === context.voteDirection) {
-              newVoteCount = comment.voteCount - context.voteDirection
+            if (comment.voteDirection === payload.voteDirection) {
+              newVoteCount = comment.voteCount - payload.voteDirection
               newVoteDirection = 0
             } else {
               // nre vote
               if (comment.voteDirection === 0) {
-                newVoteCount = comment.voteCount + context.voteDirection
+                newVoteCount = comment.voteCount + payload.voteDirection
               } else {
                 // reverse vote direction
-                newVoteCount = comment.voteCount + 2 * context.voteDirection
+                newVoteCount = comment.voteCount + 2 * payload.voteDirection
               }
-              newVoteDirection = context.voteDirection
+              newVoteDirection = payload.voteDirection
             }
             return {
               ...comment,
               voteCount: newVoteCount,
               voteDirection: newVoteDirection
             }
-          })
-        ))
+          }
+          )
+        }
+      )
+      return prevCommentsData
+    },
+    onError: (error, variables, context) => {
+      queryClient.setQueryData(['posts', variables.postId, 'comments'], context)
+      throw error
     },
     retry: 0
   })
