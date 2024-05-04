f329c921ad60060cc13f46c39b32bafd737d2cc5
Ayxa Chaverra <achaverrar@gmail.com>
2024-05-04 14:54:13 -0500

    modify the user summary styles to make them more consistent

--- src/features/users/UserSummary.jsx
+++ src/features/users/UserSummary.jsx
@@ -1,71 +1,75 @@
-import { Link } from 'react-router-dom'
-import { CountryDiccionary, ItFieldDictionary, LanguageDictionary, TechnologyDictionary } from '../editUserProfile/Dictionary'
+import React from 'react'
+import { Card, Chip } from '@material-tailwind/react'
+import { LanguageDictionary, TechnologyDictionary, ItFieldDictionary, CountryDiccionary } from '../editUserProfile/Dictionary'
+import getFullResourcesFromDictionaryByIds from '../../shared/utils/getFullResourcesFromDictionaryByIds'
 import UserAvatar from '../../shared/components/UserAvatar'
+import { BriefcaseIcon, GlobeAmericasIcon } from '@heroicons/react/24/solid'
 
 const UserSummary = ({ user }) => {
+  const country = user.countryId && CountryDiccionary.find(field => field.value === user.countryId).label
+  const itField = user.itFieldId && ItFieldDictionary.find(field => field.value === user.itFieldId).label
+  const languages = getFullResourcesFromDictionaryByIds({ ids: user.languages, dictionary: LanguageDictionary })
+  const technologies = getFullResourcesFromDictionaryByIds({ ids: user.technologies, dictionary: TechnologyDictionary })
+
   return (
-    <Link to={`/users/${encodeURIComponent(user.username)}`} key={user.id} className="max-w-3xl w-full mx-auto">
-      <div className="flex flex-col">
-        <div className="bg-white border border-white shadow-lg rounded-3xl p-4 m-4">
-          <div className="flex-none sm:flex">
-            <UserAvatar avatarUrl={user.avatarUrl} username={user.username} size="lg" />
-            <div className="flex-auto sm:ml-5 justify-evenly">
-              <div className="flex items-center justify-between sm:mt-2">
-                <div className="flex items-center">
-                  <div className="flex flex-col">
-                    <div className="w-full flex-none text-lg text-accent-dark font-bold leading-none">
-                      <span>{user.username}</span>
-                      {user.countryId && (
-                        <>
-                          <span className="text-accent-dark mx-1">·</span>
-                          <span className="text-grey-dark">{CountryDiccionary.find(country => country.value === user.countryId)?.label}</span>
-                        </>
-                      )}
-                    </div>
-                    <div className="flex text-gray-500 my-1">
-                      <span className="mr-3">{user.score} {user.score === 1 ? 'punto' : 'puntos'}</span>
-                      <div>
-                      {user.openToWork && (
-                        <>
-                          <span className="mr-3 border-r border-gray-200  max-h-0"></span>
-                          <span>Abierto a ofertas</span>
-                        </>
-                      )}
-                      </div>
-                    </div>
-                    {user.languages.length > 0 && (
-                      <div className="flex-auto text-gray-500 my-1">
-                        {user.languages.map((langId, index) => (
-                          <span key={langId}>
-                            {LanguageDictionary.find(lang => lang.value === langId)?.label}
-                            {index !== user.languages.length - 1 ? ', ' : ''}
-                          </span>
-                        ))}
-                      </div>
-                    )}
-                    {user.itFieldId && (
-                      <div className="flex-auto text-gray-500 my-1">
-                        <span>{ItFieldDictionary.find(field => field.value === user.itFieldId)?.label}</span>
-                      </div>
-                    )}
-                    {user.technologies.length > 0 && (
-                      <div className="flex-auto text-primary-dark font-bold my-1">
-                        {user.technologies.map((techId, index) => (
-                          <span key={techId}>
-                            {TechnologyDictionary.find(tech => tech.value === techId)?.label}
-                            {index !== user.technologies.length - 1 ? ', ' : ''}
-                          </span>
-                        ))}
-                      </div>
-                    )}
-                  </div>
-                </div>
-              </div>
+    <Card className='flex gap-2 w-full p-4 pt-5'>
+      <div className='flex gap-4'>
+        <div className='self-center min-w-14 min-h-14 w-full h-full max-w-24 max-h-24'>
+          <UserAvatar avatarUrl={user.avatarUrl} className="w-full h-full" />
+        </div>
+        <div className='text-grey-dark'>
+          <div>
+            <span className='font-bold text-lg text-accent-dark'>{user.username}</span>
+            <span className='font-semibold mx-2'>&middot;</span>
+            {country && <span className='font-bold text-lg text-grey-dark'>{country}</span>}
+          </div>
+          <div className='flex flex-col gap-1 mt-2 text-md'>
+            <div>
+              <span>{user.score} pts</span>
+              {
+                user.itFieldId && (
+                <>
+                  <span className='font-semibold mx-2'>&middot;</span>
+                  <span>{itField}</span>
+                </>
+                )
+              }
             </div>
+            {user.openToWork && (
+              <div className='flex items-center gap-2 font-semibold'>
+                <BriefcaseIcon className='w-4 h-4' />En busca de ofertas
+              </div>
+            )}
+            {languages.length > 0 &&
+              <div className='flex items-center flex-wrap gap-2 my-1 leading-none'>
+                <GlobeAmericasIcon className='w-4 h-4' />
+                {languages.map((lang, index) => (
+                  <React.Fragment key={lang.value}>
+                    {index !== 0 && <span className='font-semibold'>&middot;</span>}
+                    <span>{lang.label}</span>
+                  </React.Fragment>
+                ))
+                }
+              </div>
+            }
           </div>
         </div>
       </div>
-      </Link>
+      {technologies.length > 0 &&
+        <div className='flex flex-wrap gap-2 mt-2'>
+          {technologies.map((tech) => (
+            <Chip
+              key={tech.value}
+              variant="ghost"
+              value={tech.label}
+              size="sm"
+              color="blue"
+              className="normal-case text-sm rounded-full"
+              />
+          ))}
+        </div>
+      }
+    </Card>
   )
 }
 
