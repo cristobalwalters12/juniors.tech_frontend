4150009c9328822da9199fad8206f00db1b940f4
Ayxa Chaverra <achaverrar@gmail.com>
2024-05-03 20:02:52 -0500

    use skeletons as placeholders while fetching the posts data in the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,9 @@ import { useDocumentTitle } from '../shared/hooks/useDocumentTitle'
 import { useGetPosts } from '../features/posts/useGetPosts'
 import PostSummary from '../features/posts/PostSummary'
 import { useCallback, useRef } from 'react'
-import { Button, Spinner } from '@material-tailwind/react'
+import { Button } from '@material-tailwind/react'
 import { showErrorToast } from '../shared/utils/showErrorToast'
+import SkeletonList from '../shared/components/Skeletons/SkeletonList'
 
 const Home = () => {
   useDocumentTitle('Página de inicio')
@@ -35,14 +36,6 @@ const Home = () => {
     if (post) intObserver.current.observe(post)
   }, [isFetchingNextPage, fetchNextPage, hasNextPage])
 
-  if (isLoading) {
-    return (
-    <div className='flex justify-center'>
-      <Spinner className="h-16 w-16 text-gray-900/50" />
-    </div>
-    )
-  }
-
   if (isError) {
     showErrorToast(error, error.message)
     return (
@@ -60,32 +53,30 @@ const Home = () => {
   const posts = data?.pages.flatMap(page => page.posts) || []
 
   return (
-    <div className={`flex flex-col gap-3 mb-4 mr-4 ${posts.length > 0 ? 'max-w-[48rem]' : ''}`}>
-      {posts.length > 0
-        ? posts.map((post, index, { length }) => {
-          if (index === length - 1) {
-            return (
+    <div className={`flex flex-col gap-3 mb-4 mr-4 ${posts?.length > 0 ? 'max-w-[48rem]' : ''}`}>
+      {isLoading
+        ? <SkeletonList totalSkeletons={5} />
+        : posts.length > 0
+          ? posts.map((post, index, { length }) => {
+            if (index === length - 1) {
+              return (
               <Link ref={lastPostRef} key={post.id} to={`/posts/${post.id}/${post.slug}`}>
                 <PostSummary post={post} />
               </Link>
-            )
-          }
-          return (
+              )
+            }
+            return (
             <Link key={post.id} to={`/posts/${post.id}/${post.slug}`}>
               <PostSummary post={post} />
             </Link>
-          )
-        })
-        : (<div className='text-center w-full text-lg'>
+            )
+          })
+          : (<div className='text-center w-full text-lg'>
             <p>Aún no hay publicaciones en esta categoría.</p>
             <Link to="/posts/new" className='text-primary-dark font-semibold'>Anímate a crear la primera</Link>
           </div>)
     }
-      {isFetchingNextPage && (
-        <div className='flex justify-center'>
-          <Spinner className="h-16 w-16 text-gray-900/50" />
-        </div>
-      )}
+      {isFetchingNextPage && (<SkeletonList totalSkeletons={1} />)}
     </div>
   )
 }
